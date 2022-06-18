56dc0c50748cf85550cee68cb61bd61ad63b7e3a
VelociRaptor <77036902+HexM7@users.noreply.github.com>
2022-06-18 20:31:06 +0530

    Split type definitions

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,53 @@
+export type DrawFunction = (
+  context: CanvasRenderingContext2D,
+  layerImg: HTMLImageElement,
+  index: number,
+  totalFrames: number,
+) => void;
 
+export type SkipFunction = (
+  index: number,
+  totalFrames: number,
+) => boolean;
+
+export interface EncoderOptions = {
+  highWaterMark?: number
+};
+
+export type BaseLayerDraw = (
+  context: CanvasRenderingContext2D,
+  layerImg: HTMLImageElement,
+  index: number,
+  totalFrames: number,
+) => void
+
+export type LayerDraw = (
+  context: CanvasRenderingContext2D,
+  layerImg: HTMLImageElement,
+  index: number,
+  totalFrames: number,
+  layerIndex: number,
+) => void
+
+export interface BaseLayer {
+  data: Buffer | string,
+  marginLeft?: number,
+  marginTop?: number,
+  width?: number,
+  height?: number,
+  drawFunction?: DrawFunction,
+  skipFunction?: SkipFunction,
+  skipIndexes?: Array<number>,
+}
+
+export interface Layer {
+  data: Buffer | string,
+  marginLeft?: number,
+  marginTop?: number,
+  width?: number,
+  height?: number,
+  drawFunction?: DrawFunction,
+  skipFunction?: SkipFunction,
+  skipIndexes?: Array<number>,
+  disabled?: boolean,
+}
